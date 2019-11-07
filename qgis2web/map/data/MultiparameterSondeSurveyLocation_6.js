424ac40ba9a1ff2a62268d5c5e946144fd24d620
Sara Orofino <saraorofino@ESM.UCSB.EDU>
2019-11-07 15:46:44 -0800

    Add published map for hw2

---
+++ qgis2web/map/data/MultiparameterSondeSurveyLocation_6.js
@@ -0,0 +1,9 @@
+var json_MultiparameterSondeSurveyLocation_6 = {
+"type": "FeatureCollection",
+"name": "MultiparameterSondeSurveyLocation_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "id": 1.0, "site": "Pool 300", "y": -14.181764, "x": -169.658424 }, "geometry": { "type": "Point", "coordinates": [ -169.658424, -14.181764 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "id": 2.0, "site": "Olosega", "y": -14.180778, "x": -169.622767 }, "geometry": { "type": "Point", "coordinates": [ -169.622767, -14.180778 ] } }
+]
+}
---
+++ qgis2web/map/data/TemperatureLoggerLocations_4.js
@@ -0,0 +1,15 @@
+var json_TemperatureLoggerLocations_4 = {
+"type": "FeatureCollection",
+"name": "TemperatureLoggerLocations_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "id": 1.0, "site": "Olosega", "y": -14.178813, "x": -169.62485, "logger_id": "olosega_2" }, "geometry": { "type": "Point", "coordinates": [ -169.62485, -14.178813 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "id": 2.0, "site": "Olosega", "y": -14.180025, "x": -169.62353, "logger_id": "olosega_3" }, "geometry": { "type": "Point", "coordinates": [ -169.62353, -14.180025 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3", "id": 3.0, "site": "Olosega", "y": -14.180035, "x": -169.62353, "logger_id": "olosega_3new" }, "geometry": { "type": "Point", "coordinates": [ -169.62353, -14.180035 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4", "id": 4.0, "site": "Olosega", "y": -14.179285, "x": -169.62428, "logger_id": "olosega_68" }, "geometry": { "type": "Point", "coordinates": [ -169.62428, -14.179285 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "5", "id": 5.0, "site": "Pool 200", "y": -14.18462, "x": -169.66507, "logger_id": "pool200_nr" }, "geometry": { "type": "Point", "coordinates": [ -169.66507, -14.18462 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "6", "id": 6.0, "site": "Pool 300", "y": -14.182444, "x": -169.658738, "logger_id": "pool300_6" }, "geometry": { "type": "Point", "coordinates": [ -169.658738, -14.182444 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "7", "id": 7.0, "site": "Pool 300", "y": -14.182067, "x": -169.65829, "logger_id": "pool300_8" }, "geometry": { "type": "Point", "coordinates": [ -169.65829, -14.182067 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "8", "id": 8.0, "site": "Pool 300 ", "y": -14.1816, "x": -169.658, "logger_id": "pool300_light" }, "geometry": { "type": "Point", "coordinates": [ -169.658, -14.1816 ] } }
+]
+}
